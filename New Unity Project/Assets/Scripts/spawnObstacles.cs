f5473330be56ef1e2ae3e3222d556267efc94a6e
itamar berti <itamar.berti8@gmail.com>
2021-11-11 15:04:38 +0200

    Obstacle

---
+++ New Unity Project/Assets/Scripts/spawnObstacles.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnObstacles : MonoBehaviour
+{
+    public GameObject obstacle;
+    public float maxX;
+    public float minX;
+    public float maxY;
+    public float minY;
+    public float timeBetweenSpawns;
+    public float spawnTime;
+
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > spawnTime)
+        {
+            spawn();
+            spawnTime = Time.time + timeBetweenSpawns;
+        }
+    }
+    void spawn()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+
+        Instantiate(obstacle, transform.position + new Vector3(randomX, randomY, 0), transform.rotation);
+    }
+}
